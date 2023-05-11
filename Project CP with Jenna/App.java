aaab8b457ab099a1b44bb39a038bb45a8e699b58
mlxxx63 <105572717+mlxxx63@users.noreply.github.com>
2023-05-11 20:57:23 +0700

    Update App.java

--- Project CP with Jenna/App.java
+++ Project CP with Jenna/App.java
@@ -10,6 +10,10 @@ public class App
         {
             showMenu();
 
+            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Enter what you want to do: ");
+            int input = myObj.nextInt();
+
             if (input == 1)
             {
                 Scanner myObj2 = new Scanner(System.in);  
@@ -19,24 +23,28 @@ public class App
 
                 for (int i = 0; i < numberPlayList; i ++)
                 {
+                    System.out.println("Enter the infos for PlayList " + i + 1);
                     playList[i] = new PlayList();
                     playList[i].askName();
                     playList[i].askGenre();
-                    System.out.println(playList[i].getName());
-                    System.out.println(playList[i].getGenre());
                 }
             }
         }
     }
-    public void showMenu()
-    {
-        
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter what you want to do: ");
-        int input = myObj.nextInt();
-    }
-}
 
+    public static void showMenu()
+    { 
+        System.out.println("");
+        System.out.println("Main Mene: ");
+        System.out.println("---------------------------------------");
+        System.out.println("|   Option 1: Create playlist         |");
+        System.out.println("|   Option 2: Add songs to playlist   |");
+        System.out.println("|   Option 3: Show playlist           |");
+        System.out.println("|   Option 7: Exit                    |");
+    }
+
+    
+}
 
 public class PlayList
 {
