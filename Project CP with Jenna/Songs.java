3f55fa47144b5c48c148d12e2e3ef302c2e88945
Panda6658 <132931580+Panda6658@users.noreply.github.com>
2023-05-16 09:59:17 -0300

    Update Songs.java

--- Project CP with Jenna/Songs.java
+++ Project CP with Jenna/Songs.java
@@ -1,81 +1,45 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-public class App
+public class Songs
 {
-    public static void main(String[] args)
+    public Songs()
     {
-        PlayList[] playList = null;
-
-        while(true)
-        {
-            showMenu();
-
-            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-            System.out.println("Enter what you want to do: ");
-            int input = myObj.nextInt();
-
-            if (input == 1)
-            {
-                Scanner myObj2 = new Scanner(System.in);  
-                System.out.println("How many playlist you want to make: ");
-                int numberPlayList = myObj2.nextInt();
-                playList = new PlayList[numberPlayList];
-
-                for (int i = 0; i < numberPlayList; i ++)
-                {
-                    System.out.println("Enter the infos for PlayList " + i + 1);
-                    playList[i] = new PlayList();
-                    playList[i].askName();
-                    playList[i].askGenre();
-                }
-            }
-            if (input == 2)
-            {
-                getSong();
-            }
-        }
-
-        
-        
-        
-        
-        //mine
-        
     }
     
-    public static void getSong()
+    private static String genre;
+    private static  String artist;
+    private static String name;
+    
+    //setter
+    public static void setName(String theName)
     {
-        ArrayList<String> list = new ArrayList<String>();
-        System.out.println("\'Print stop when done\'");
-        while(true)
-        {
-            Scanner scan = new Scanner(System.in);
-            System.out.println("Enter Song name: ");
-            String answer = scan.nextLine();
-            Songs.setName(answer);
-            
-            if(answer.equalsIgnoreCase("stop"))
-            {
-                break;
-            }
-            
-            //Scanner input_genre = new Scanner(System.in);
-            System.out.println("Enter artist name: ");
-            String genre = scan.nextLine();
-            //if
-            Songs.setGenre(genre);
-            list.add(answer); //needs to be song with song info
-        }
+        name = theName;
     }
     
-    public static void showMenu()
-    { 
-        System.out.println("");
-        System.out.println("Main Mene: ");
-        System.out.println("---------------------------------------");
-        System.out.println("|   Option 1: Create playlist         |");
-        System.out.println("|   Option 2: Add songs to playlist   |");
-        System.out.println("|   Option 3: Show playlist           |");
-        System.out.println("|   Option 7: Exit                    |");
+    public static void setArtist(String artistName)
+    {
+        artist = artistName;
     }
+    
+    public static void setGenre(String type)
+    {
+        genre = type;
+    }
+    
+    //getter
+    public String getSongGenre()
+    {
+        return genre;
+    }
+    
+    public String getArtist()
+    {
+        return artist;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    
 }
